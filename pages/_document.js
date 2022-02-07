f77b6e955d80c8b07f49575c3040728d78c41c37
emrekesler <emrekesler@outlook.com>
2022-02-07 12:50:43 +0300

    Font import directly

--- pages/_document.js
+++ pages/_document.js
@@ -4,14 +4,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Inter&display=optional" rel="stylesheet" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.4/tiny-slider.css" />
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,18 +11,11 @@ module.exports = {
     },
     extend: {
       fontFamily: {
-        rubik: "'Rubik', serif",
+        rubik: "'Rubik', sans-serif",
       },
       fontSize: {
         tiny: ".800rem",
         calc: "calc(1.265625rem + 0.1875vw)",
-      },
-      backgroundImage: {
-        blog1: "url('https://blogzine.webestica.com/assets/images/blog/1by1/01.jpg')",
-        blog2: "url('https://blogzine.webestica.com/assets/images/blog/4by3/01.jpg')",
-        coin: "url('https://images.unsplash.com/photo-1643488056768-fe017dc164b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80')",
-        food: "url('https://images.unsplash.com/photo-1632158930341-46604b637a0f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80')",
-        car: "url('https://img.redbull.com/images/w_2929/q_auto,f_auto/redbullcom/2021/10/10/pu9vpczmlgzwilqrlufm/red-bull-racing')",
       },
       spacing: {
         128: "35rem",
